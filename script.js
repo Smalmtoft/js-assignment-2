f63ac05050817789e018d17442ace1d694caadcf
Sandra Malmtoft <sandra.malmtoft@gmail.com>
2025-03-19 20:34:42 +0100

    adjustments made to make promt work

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//let priceInput = prompt(`Enter your price here`);
-
-let priceInput = `$100`;
+let priceInput = prompt(`Enter your price here`);
 
 let price = parseFloat(priceInput.slice(1));
 
 let discount = price * 0.9;
 
-console.log(`Your new price is $${discount}`);
+console.log(`Your new price is $${discount.toFixed(2)}`);
