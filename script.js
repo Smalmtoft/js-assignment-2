c6ebe73b57b6912ec453fbd6cebb39b445f883f4
Sandra Malmtoft <sandra.malmtoft@gmail.com>
2025-03-19 12:41:05 +0100

    content added to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+//let priceInput = prompt(`Enter your price here`);
+
+let priceInput = `$100`;
+
+let price = parseFloat(priceInput.slice(1));
+
+let discount = price * 0.9;
+
+console.log(`Your new price is ${discount}$`);
